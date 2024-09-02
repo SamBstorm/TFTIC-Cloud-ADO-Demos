c931e2ae29a76c6552191b132d8dd8d8628d3017
Samuel Legrain <samuel.legrain@bstorm.be>
2024-09-02 17:03:38 +0200

    Add Connection

---
+++ Demo-ADO01/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Data.SqlClient;
+
+namespace Demo_ADO01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Récupérer la ConnectionString dans VS2022
+            //Dans explorateur de serveur, retrouver serveur voulu (ou le connecter)
+            //Cliquez dessus et choisir propriétés
+            //Dans les propriétés copiez "Chaine de connection"
+            string connectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=DBSlide;Integrated Security=True;Encrypt=False;";
+
+            using (SqlConnection connection = new SqlConnection())
+            {
+                connection.ConnectionString = connectionString;
+                Console.WriteLine(connection.State);
+                connection.Open();
+                Console.WriteLine(connection.State);
+                connection.Close();
+                Console.WriteLine(connection.State);
+            }
+        }
+    }
+}

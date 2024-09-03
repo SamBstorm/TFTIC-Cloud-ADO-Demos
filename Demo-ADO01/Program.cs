bf3d8a70a047b5ec85d5475664cffd931bda3b7a
Samuel Legrain <samuel.legrain@bstorm.be>
2024-09-03 16:11:42 +0200

    Ajout Demo DML SqlParameter

--- Demo-ADO01/Program.cs
+++ Demo-ADO01/Program.cs
@@ -55,28 +55,73 @@ namespace Demo_ADO01
             #endregion
 
             #region Démo - SqlDataAdapter (mode déconnecté)
+            //using (SqlConnection connection = new SqlConnection(connectionString))
+            //{
+            //    using (SqlCommand command = connection.CreateCommand())
+            //    {
+            //        command.CommandText = "SELECT student_id, last_name, first_name FROM student";
+            //        SqlDataAdapter adapter = new SqlDataAdapter();
+            //        adapter.SelectCommand = command;
+
+            //        DataTable table = new DataTable();
+            //        adapter.Fill(table);
+
+            //        foreach (DataRow row in table.Rows)
+            //        {
+            //            var result = new {
+            //                student_id = (int)row[table.Columns["student_id"]],
+            //                last_name = row[table.Columns[1]].ToString(),
+            //                first_name = row["first_name"].ToString()};
+            //            Console.WriteLine($"{result.student_id} - {result.first_name} {result.last_name}");
+            //        }
+            //    }
+            //}
+            #endregion
+
+            #region Démo - Insert - Update - Delete - With Parameters
+
+            Section nouvelleSection = new Section
+            {
+                Section_id = 2627,
+                Section_name = null,
+                Delegate_id = null
+            };
+
             using (SqlConnection connection = new SqlConnection(connectionString))
             {
                 using (SqlCommand command = connection.CreateCommand())
                 {
-                    command.CommandText = "SELECT student_id, last_name, first_name FROM student";
-                    SqlDataAdapter adapter = new SqlDataAdapter();
-                    adapter.SelectCommand = command;
-
-                    DataTable table = new DataTable();
-                    adapter.Fill(table);
-
-                    foreach (DataRow row in table.Rows)
+                    command.CommandText = "INSERT INTO section VALUES (@Section_id, @Section_name, @Delegate_id)";
+                    SqlParameter p1 = new SqlParameter()
                     {
-                        var result = new {
-                            student_id = (int)row[table.Columns["student_id"]],
-                            last_name = row[table.Columns[1]].ToString(),
-                            first_name = row["first_name"].ToString()};
-                        Console.WriteLine($"{result.student_id} - {result.first_name} {result.last_name}");
+                        ParameterName = nameof(Section.Section_id),
+                        Value = nouvelleSection.Section_id
+                    };
+                    command.Parameters.Add(p1);
+
+                    SqlParameter p2 = command.CreateParameter();
+                    p2.ParameterName = nameof(Section.Section_name);
+                    p2.Value = (object)nouvelleSection.Section_name ?? DBNull.Value;
+                    command.Parameters.Add(p2);
+
+                    command.Parameters.AddWithValue(
+                        nameof(Section.Delegate_id), 
+                        (object)nouvelleSection.Delegate_id ?? DBNull.Value);
+
+                    connection.Open();
+                    int row = command.ExecuteNonQuery();
+                    if (row > 0)
+                    {
+                        Console.WriteLine("Sauvegarde bien effectuée");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Sauvegarde non-effectuée");
                     }
                 }
+
+                #endregion
             }
-            #endregion
         }
     }
 }
---
+++ Demo-ADO01/Section.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo_ADO01
+{
+    internal class Section
+    {
+        public int Section_id { get; set; }
+        public string? Section_name { get; set; }
+        public int? Delegate_id { get; set; }
+    }
+}
